4e006cd3eeccd3e27bf48f3f7a67dd34257f1577
Vivek <vratkalkar@live.com>
2014-05-16 14:44:26 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,8 @@ Project1::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host =&gt; 'vr-wikiapp.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'vr-wikiapp.herokuapp.com'}
+
 
   config.action_mailer.delivery_method = :smtp
 
