43d9840180ff3a4d8bba5c82c7e8cee6a1606665
Vivek Ratkalkar <vratkalkar@live.com>
2014-06-26 11:31:34 -0700

    update session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Project1::Application.config.session_store :cookie_store, key: '_project1_session'
+Project1::Application.config.session_store :cookie_store, key: '_project1_session', expire_after: 2.minutes
