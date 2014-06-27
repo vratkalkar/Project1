3c932e54b2bc12846bb59923f78b6b976733de76
Vivek Ratkalkar <vratkalkar@live.com>
2014-06-26 18:54:24 -0700

    added email confirmation skip for OAuth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
+    @user.skip_confirmation!
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,11 +19,14 @@ class User < ActiveRecord::Base
 
  def self.find_for_facebook_oauth(auth)
   where(auth.slice(:provider,:uid)).first_or_create do |user|
+    user.skip_confirmation!
     user.provider = auth.provider
     user.uid      = auth.uid
     user.email    = auth.info.email
     user.password = Devise.friendly_token[0,20]
     user.name     = auth.info.name
+    user.confirm!
+    user.save
    end
   end
 
